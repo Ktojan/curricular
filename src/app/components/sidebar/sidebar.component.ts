dc8bc62c0c7e742f064d28607122f4f0f3574381
Andrey <karpovich.andrey@gmail.com>
2024-10-18 13:37:59 +0300

    osm-map plugin integration

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { IProfile } from '../../models/profile';
 import { DataService } from '../../services/data.service';
 import { EMPTY_PROFILE } from '../../shared/defaults';
+import {osmMap} from '../../shared/osm-map-plugin';
+
+const lisbonCoords = [-9.1562, 38.74054];
 
 @Component({
   selector: 'sidebar',
@@ -9,13 +12,20 @@ import { EMPTY_PROFILE } from '../../shared/defaults';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
+  @ViewChild('mapchunk') mapchunkElem: ElementRef<HTMLElement>;
 
   public profile: IProfile = EMPTY_PROFILE;
+
   constructor(private dataService:DataService) { }
 
   ngOnInit() {
     this.dataService.url = DataService.DATA;
     this.dataService.getSection('profile').subscribe((data: IProfile) => this.profile = data);
+  }
+
+  ngAfterViewInit() {
+    const mapChunkHTL = osmMap(lisbonCoords[0], lisbonCoords[1], 11, 300, 250); // lng, lat, zoom, width, height
+    this.mapchunkElem.nativeElement.innerHTML = mapChunkHTL;
   }
 
 }
